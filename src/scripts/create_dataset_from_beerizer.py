546d6c9129168c81e646cce7f04e6b48c0149bbf
owhenthesaints <owen.simon@epfl.ch>
2024-11-21 14:35:54 +0100

    improved code stability + get largest page

--- src/scripts/create_dataset_from_beerizer.py
+++ src/scripts/create_dataset_from_beerizer.py
@@ -5,6 +5,8 @@ import os
 import time
 from dotenv import load_dotenv
 from bs4 import BeautifulSoup
+import sys
+import numpy as np
 
 load_dotenv()
 url = 'https://beerizer.com/?page1'
@@ -20,51 +22,74 @@ driver = webdriver.Firefox(service=Service(os.path.join(path_to_firefox, 'geckod
 driver.get(url)
 
 
+
 time.sleep(0.1)
 
-beer_sections = BeautifulSoup(driver.page_source, 'html.parser').find_all('div', class_='beer-inner-top')
 
-for beer_section in beer_sections:
-    # get the name of the beer
-    title_span = beer_section.find('span', class_='title', attrs={'itemprop': 'name'})
+def get_largest_page(page_source):
+    max_page = 0
+    for page_switch_el in page_source.find_all('a', class_='dynamic-url scroll-to-beers exact-link'):
+        try:
+            largest_page = int(page_switch_el.get_text(strip=True)) > max_page
+        except ValueError:
+            continue
+        if largest_page:
+            max_page = int(page_switch_el.get_text(strip=True))
+    return max_page
 
-    if title_span:
-        strong_text = title_span.find('strong')
-        print(strong_text.text.strip())
 
-    brewery_span = beer_section.find('span', class_='brewery-title')
+try:
+    page_source = BeautifulSoup(driver.page_source, 'html.parser')
 
-    if brewery_span:
-        img = brewery_span.find('img')
-        if img:
-            print(img.get('alt'))
-            img.extract()
-        print(brewery_span.get_text(strip=True))
+    beer_sections = page_source.find_all('div', class_='beer-inner-top')
 
-    price_span = beer_section.find('span', class_='price')
-    if price_span:
-        price = price_span.text.strip()
-        print(price)
+    print("number of pages ",get_largest_page(page_source))
+    for beer_section in beer_sections:
+        # get the name of the beer
+        title_span = beer_section.find('span', class_='title', attrs={'itemprop': 'name'})
+        if title_span:
+            strong_text = title_span.find('strong')
+            print(strong_text.text.strip())
 
-    pack_info_div = beer_section.find('div', class_='pack-info')
-    if pack_info_div:
-        span_pack_info_div = pack_info_div.find('span')
-        if span_pack_info_div:
-            print(span_pack_info_div.get_text(strip=True))
-            print(span_pack_info_div.get('title'))
+        brewery_span = beer_section.find('span', class_='brewery-title')
+        if brewery_span:
+            img = brewery_span.find('img')
+            if img:
+                print(img.get('alt'))
+                img.extract()
+            print(brewery_span.get_text(strip=True))
 
-    additional_info = beer_section.find('div', class_='right-item-row rating-abv-rpc')
-    untapped_rating = additional_info.find('a', class_='untappd untappd-mouseover')
-    if untapped_rating:
-        print(untapped_rating.get_text(strip=True))
+        price_span = beer_section.find('span', class_='price')
+        if price_span:
+            price = price_span.text.strip()
+            print(price)
 
-    percentage_alcohol = additional_info.find('span', class_='abv value')
-    if percentage_alcohol:
-        print(percentage_alcohol.get_text(strip=True))
+        pack_info_div = beer_section.find('div', class_='pack-info')
+        if pack_info_div:
+            span_pack_info_div = pack_info_div.find('span')
+            if span_pack_info_div:
+                print(span_pack_info_div.get_text(strip=True))
+                print(span_pack_info_div.get('title'))
 
-    beer_type_div = beer_section.find('div', class_='right-item-row')
-    if beer_type_div:
-        print(beer_type_div.get_text(strip=True))
+        additional_info = beer_section.find('div', class_='right-item-row rating-abv-rpc')
+        if additional_info:
+            untapped_rating = additional_info.find('a', class_='untappd untappd-mouseover')
+            if untapped_rating:
+                print(untapped_rating.get_text(strip=True))
 
-    print()
-driver.quit()
+            percentage_alcohol = additional_info.find('span', class_='abv value')
+            if percentage_alcohol:
+                print(percentage_alcohol.get_text(strip=True))
+        elif beer_section.find('div', class_='bundle-header'):
+                continue
+
+
+        beer_type_div = beer_section.find('div', class_='right-item-row style')
+        if beer_type_div:
+            print(beer_type_div.get_text(strip=True))
+
+        print()
+finally:
+    driver.quit()
+
+sys.exit()
