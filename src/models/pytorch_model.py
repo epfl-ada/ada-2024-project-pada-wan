5c0023555c3a5a82279e534dea9af410f6fb38ec
owhenthesaints <owen.simon@epfl.ch>
2024-12-18 01:25:17 +0100

    test(pytorch_model): trying to get my models fault on empty value

---
+++ src/models/pytorch_model.py
@@ -0,0 +1,138 @@
+# code heavily inspired by https://pytorch.org/tutorials/intermediate/char_rnn_classification_tutorial.html
+import pandas as pd
+import torch
+from torch.utils.data import Dataset
+import string
+import unicodedata
+import torch.nn as nn
+import torch.nn.functional as F
+import random
+import numpy as np
+import time
+from dotenv import load_dotenv
+import os
+
+allowed_characters = string.ascii_letters + ".,;''"
+n_letters = len(allowed_characters)
+
+
+def unicodeToAscii(s):
+    return ''.join(
+        c for c in unicodedata.normalize('NFD', s)
+        if unicodedata.category(c) != 'Mn'
+        and c in allowed_characters
+    )
+
+
+def letterToIndex(letter):
+    return allowed_characters.find(letter)
+
+
+def lineToTensor(line):
+    tensor = torch.zeros(len(line), 1, n_letters)
+    for li, letter in enumerate(line):
+        tensor[li][0][letterToIndex(letter)] = 1
+    return tensor
+
+
+class NamesDataset(Dataset):
+    def __init__(self, data_dir, brewery_dir):
+        data_df = pd.read_csv(data_dir)
+        data_df['beer_name'] = data_df['beer_name'].apply(unicodeToAscii)
+        brewery_df = pd.read_csv(brewery_dir)
+        brewery_ids = data_df['brewery_id'].tolist()
+        id_to_location = dict(zip(brewery_df['id'], brewery_df['location']))
+        print(data_df[data_df['beer_name'].isna()])
+
+        # get the data to list
+        self.data = data_df['beer_name'].tolist()
+        self.data_tensor = data_df['beer_name'].apply(lineToTensor).tolist()
+        self.labels = [id_to_location[id] for id in brewery_ids]
+        self.labels_tensor = []
+        labels_set = set(self.labels)
+        # gives you the unique labels
+        self.labels_unique = list(labels_set)
+        for idx in range(len(self.labels)):
+            self.labels_tensor.append(torch.tensor([self.labels_unique.index(self.labels[idx])], dtype=torch.long))
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        return self.labels_tensor[idx], self.data_tensor[idx], self.labels[idx], self.data[idx]
+
+
+
+class CharRNN(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size):
+        super(CharRNN, self).__init__()
+        self.rnn = nn.RNN(input_size, hidden_size)
+        self.h2o = nn.Linear(hidden_size, output_size)
+        self.softmax = nn.LogSoftmax(dim=1)
+
+    def forward(self, line_tensor):
+        rnn_out, hidden = self.rnn(line_tensor)
+        output = self.h2o(hidden[0])
+        output = self.softmax(output)
+        return output
+
+
+def train(rnn, training_data, n_epoch = 10, n_batch_size = 64, report_every = 50, learning_rate = 0.2, criterion = nn.NLLLoss()):
+    """
+    Learn on a batch of training_data for a specified number of iterations and reporting thresholds
+    """
+    # Keep track of losses for plotting
+    current_loss = 0
+    all_losses = []
+    rnn.train()
+    optimizer = torch.optim.SGD(rnn.parameters(), lr=learning_rate)
+
+    start = time.time()
+    print(f"training on data set with n = {len(training_data)}")
+
+    for iter in range(1, n_epoch + 1):
+        rnn.zero_grad() # clear the gradients
+
+        # create some minibatches
+        # we cannot use dataloaders because each of our names is a different length
+        batches = list(range(len(training_data)))
+        random.shuffle(batches)
+        batches = np.array_split(batches, len(batches) //n_batch_size )
+
+        for idx, batch in enumerate(batches):
+            batch_loss = 0
+            for i in batch: #for each example in this batch
+                (label_tensor, text_tensor, label, text) = training_data[i]
+                output = rnn.forward(text_tensor)
+                loss = criterion(output, label_tensor)
+                batch_loss += loss
+
+            # optimize parameters
+            batch_loss.backward()
+            nn.utils.clip_grad_norm_(rnn.parameters(), 3)
+            optimizer.step()
+            optimizer.zero_grad()
+
+            current_loss += batch_loss.item() / len(batch)
+
+        all_losses.append(current_loss / len(batches) )
+        if iter % report_every == 0:
+            print(f"{iter} ({iter / n_epoch:.0%}): \t average batch loss = {all_losses[-1]}")
+        current_loss = 0
+
+    return all_losses
+
+if __name__ == "__main__":
+    load_dotenv()
+    dataset = NamesDataset(data_dir=os.getenv("PATH_TO_BEERADVOCATE_BEERS"), brewery_dir=os.getenv("PATH_TO_BEERADVOCATE_BREWERIES"))
+    train_set, test_set = torch.utils.data.random_split(dataset, [.85, .15], generator = torch.Generator().manual_seed(2024))
+    n_hidden = 128
+    rnn = CharRNN(n_letters, n_hidden, len(dataset.labels_unique))
+    start = time.time()
+    all_losses = train(rnn, train_set, n_epoch=27, report_every=5, learning_rate=0.15)
+    end = time.time()
+    print(f"training took {end-start:.2f} seconds")
+
+
+
+
--- src/models/some_model.py
+++ src/models/some_model.py
@@ -1,1 +1,26 @@
 # importing models, making pipelines...
+import torch
+from torch.utils.data import DataLoader, Dataset
+from transformers import BertTokenizer
+
+
+class BeerDataset(Dataset):
+    """
+    A dataset implements 2 functions
+        - __len__  (returns the number of samples in our dataset)
+        - __getitem__ (returns a sample from the dataset at the given index idx)
+    """
+
+    def __init__(self, dataset_parameters, dataset: list):
+        super().__init__()
+        self.dataset_parameters = dataset_parameters
+        self.data = dataset
+
+    def load_data(self):
+        pass
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        return self.data[idx]
